b96dddccabe3ef7c385131e5af9bb77dab4af791
pramodbharti <34020494+pramodbharti@users.noreply.github.com>
2018-03-02 07:16:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,6 @@ element.innerHTML = "Happy Holi";
 
 // Move the image
 var img = document.getElementById("madi");
-img.onClick = function () {
+img.onclick = function () {
     img.style.marginLeft = '200px';    
 };
