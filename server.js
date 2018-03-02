dd765059bf57922afb33ab580542ab2c819b3223
pramodbharti <34020494+pramodbharti@users.noreply.github.com>
2018-03-02 06:56:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,16 +88,12 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function(req, res){
     // articleName == article-one
     // articles[articleName] == {} content object for article one
-   var articleName = req.params.articleName 
+   var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName])); 
 });
 
-app.get('/article-two', function(req, res){
-   res.send(createTemplate(articleTwo));
-});
-
-app.get('/article-three', function(req, res){
-   res.send(createTemplate(articleThree));
+app.get('/ui/main.js', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'main.js')); 
 });
 
 app.get('/ui/style.css', function (req, res) {
